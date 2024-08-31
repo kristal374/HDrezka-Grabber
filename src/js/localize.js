74c3b05a1452fa23fb0a349177bf94b068697683
kristal <aleks.golovany@gmail.com>
2024-08-31 14:47:53 +0300

    Removed old logic, changes in building configs

--- src/js/localize.js
+++
@@ -1,8 +0,0 @@
-(() => {
-    const elements = document.querySelectorAll("[data-resource]");
-    elements.forEach(function (el) {
-        el.innerText = browser.i18n.getMessage(
-            el.getAttribute("data-resource")
-        );
-    });
-})();
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     build: {
         target: "es2022",
+        minify: false,
         rollupOptions: {
+            input: {
+                app: 'src/html/popup.html',
+            },
             output: {
-                assetFileNames: 'assets/[ext]/[name][extname]',
-                chunkFileNames: 'assets/[chunks]/[name].[hash].js',
-                entryFileNames: 'assets/js/index.js'
+                assetFileNames: 'src/[ext]/[name][extname]',
+                chunkFileNames: 'src/[chunks]/[name].[hash].js',
+                entryFileNames: 'src/js/popup.js'
             }
         }
     }
