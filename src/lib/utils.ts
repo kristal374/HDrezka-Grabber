2ccb1e2018ad1da802ea84f3f223c4476c0c27f1
Igor <hello.lr0pb@gmail.com>
2025-02-16 12:12:44 +0200

    Add `tailwind-merge` & 2 `active` color variants

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,9 @@
 import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 import type { Seasons } from './types';
 
 export function cn(...inputs: ClassValue[]) {
-  return clsx(inputs);
+  return twMerge(clsx(inputs));
 }
 
 export function sliceSeasons(
