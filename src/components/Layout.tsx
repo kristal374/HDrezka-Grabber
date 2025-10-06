ad8448e97de24b14a9cf0801c3ee7b7b8c1cb0c2
kristal <aleks.golovany@gmail.com>
2025-10-06 23:12:48 +0300

    Fix width in firefox

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 export function Layout({ children }: React.PropsWithChildren) {
   return (
-    <div className='border-popup-border flex min-h-[10.5rem] min-w-[21rem] flex-col border-8 p-3'>
+    <div className='flex flex-col border-popup-border min-h-[10.5rem] w-[21rem] min-w-[21rem] border-8 p-3'>
       {children}
     </div>
   );
