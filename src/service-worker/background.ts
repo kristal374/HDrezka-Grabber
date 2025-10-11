9ab06a7e54675608d0d202cbbc2213e2a88ec4d1
kristal <aleks.golovany@gmail.com>
2025-10-11 11:17:29 +0300

    Move parsing episodes logic

--- src/service-worker/background.ts
+++ src/service-worker/background.ts
@@ -4,20 +4,13 @@ import { logCreate } from './background-logger';
 import { doDatabaseStuff } from '@/lib/idb-storage';
 import { isFirstRunExtension } from '@/lib/on-extension-start';
 import { getSettings } from '@/lib/storage';
-import {
-  ActualVideoData,
-  DataForUpdate,
-  EventType,
-  Message,
-  ResponseVideoData,
-  SeasonsWithEpisodesList,
-  Settings,
-  SubtitleInfo,
-} from '@/lib/types';
+import { EventType, Message, Settings } from '@/lib/types';
 import { clearCache } from '@/service-worker/cache';
+import { updateVideoInfo } from '@/service-worker/response-parser';
 import type { Runtime } from 'webextension-polyfill';
 import { DownloadManager } from './load-manager/core';
-import { fetchUrlSizes, updateVideoData } from './network-layer';
+import { fetchUrlSizes } from './network-layer';
+
 type MessageSender = Runtime.MessageSender;
 
 let downloadManager: DownloadManager;
@@ -119,60 +112,6 @@ async function messageHandler(
       logger.warning(message);
       return false;
   }
-}
-
-async function extractSeasons(seasons: string) {
-  const seasonsStorage: SeasonsWithEpisodesList = {};
-  const seasonsList = seasons!
-    .split('</li>')
-    .filter((item) => item.trim() !== '');
-  seasonsList.forEach((line) => {
-    const [_, seasonID, seasonTitle] = line.match(/data-tab_id="(\d+)">(.*)$/)!;
-    seasonsStorage[seasonID] = { title: seasonTitle, episodes: [] };
-  });
-  return seasonsStorage;
-}
-
-async function extractAllEpisodes(serverResponse: ResponseVideoData) {
-  const seasonsStorage: SeasonsWithEpisodesList = await extractSeasons(
-    serverResponse.seasons!,
-  );
-
-  let episodesBlock = serverResponse
-    .episodes!.split(`</ul>`)
-    .filter((item) => item.trim() !== '');
-
-  episodesBlock.forEach((block) => {
-    const episodesList = block
-      .split(`</li>`)
-      .filter((item) => item.trim() !== '');
-    episodesList.forEach((line) => {
-      const [_, seasonID, episodeId, EpisodeTitle] = line.match(
-        /data-season_id="(\d+)" data-episode_id="(\d+)">(.*)$/,
-      )!;
-      seasonsStorage[seasonID].episodes.push({
-        id: episodeId,
-        title: EpisodeTitle,
-      });
-    });
-  });
-  return seasonsStorage;
-}
-
-async function updateVideoInfo(data: DataForUpdate): Promise<ActualVideoData> {
-  logger.debug('Request to update video data:', data);
-  const serverResponse = await updateVideoData(data.siteURL, data.movieData);
-  return {
-    seasons: serverResponse?.seasons
-      ? await extractAllEpisodes(serverResponse)
-      : null,
-    subtitle: {
-      subtitle: serverResponse.subtitle,
-      subtitle_def: serverResponse.subtitle_def,
-      subtitle_lns: serverResponse.subtitle_lns,
-    } as SubtitleInfo,
-    streams: serverResponse.url,
-  };
 }
 
 const handleError = async (originalError: Error) => {
